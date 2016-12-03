b7bd73ba2d3cc708396a09f0734465513ba7e7ec
Daniel <danielu96@hotmail.com>
2016-12-03 16:16:09 +0100

    viewExistingCase

---
+++ src/prototypefys/viewExistingCase.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototypefys;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Daniel
+ */
+public class viewExistingCase {
+    
+    viewExistingCase(){
+        
+    }
+    
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("JavaFX Welcome");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Enter Case ID#");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Case ID#:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Scene scene = new Scene(grid, 300, 275);
+        
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Button btn1 = new Button("View baggage catalogue");
+        grid.add(btn1, 1, 2);
+        btn1.setText("View baggage catalogue");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("X");
+            }
+        });
+        
+        Button btn2 = new Button("View baggage catalogue");
+        grid.add(btn2, 1, 2);
+        btn2.setText("hoi");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+
+        @Override
+        public void handle(ActionEvent event) {
+              System.out.println("X");
+            }
+        });
+        
+
+        
+}
+    
+}
