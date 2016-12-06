c35dc90b3ae00244c144a31bc77febf723fe98ce
Michael <mikey0812@gmail.com>
2016-12-06 11:34:50 +0100

    EmployeeLog Main Removed/Package change

--- src/prototypefys/DisplayEmployeeLog.java
+++ src/prototypefys/DisplayEmployeeLog.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DisplayEmployeeLogs;
+package prototypefys;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
@@ -98,8 +98,5 @@ public class DisplayEmployeeLog extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        launch(args);
-    }
     
 }
